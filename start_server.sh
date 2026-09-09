5e6e98193432f3c2622bd67c67681fe941bcb833
Tom Martinson <tomemartinson@gmail.com>
2026-09-09 12:30:09 -0700

    Mount bucket directory for log storage in start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,9 +1,15 @@
+#store logs locally
+#LOG_STORAGE_PATH=`pwd`/data
+
+#store logs in the google bucket
+LOG_STORAGE_PATH=`pwd`/bucket
+
 sudo docker run -d \
         --name=flight-review \
         --restart=always \
         --network=host \
         -e PORT=80 \
         -e BOKEH_ALLOW_WS_ORIGIN=flight-review.modalai.com \
-        -v `pwd`/data:/opt/service/data \
+        -v $LOG_STORAGE_PATH:/opt/service/data \
         px4flightreview
 
